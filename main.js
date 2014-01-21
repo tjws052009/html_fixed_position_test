683e3f31ae0971cb99e807de32137644feb02017
Tomohiro Sugimoto <sugimoto@netprice.com>
2014-01-21 13:28:14 +0900

    init

---
+++ main.js
@@ -0,0 +1,30 @@
+$(function() {
+  setOffset();
+
+  var timerClear = true;
+  $(window).resize(function() {
+    if (timerClear == true) {
+      setOffset();
+      timerClear = false;
+      setTimeout( function() {
+        timerClear = true;
+      }, 200)
+    }
+  });
+});
+
+
+
+// ===========================
+setOffset = function() {
+  m_w = $('.main').width();
+  console.debug(m_w);
+  
+  m_l = $('.main').offset().left;
+  console.debug(m_l);
+
+  o_w = $('.follow').width();
+  console.debug(o_w);
+
+  $('.follow').css('left', (m_w + m_l - o_w) + 'px');
+}
